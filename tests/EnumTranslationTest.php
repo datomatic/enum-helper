5ad8fa17342ae028bb66de2edf42044b827a25bc
PHP CS Fixer <phpcsfixer@example.com>
2022-05-25 10:53:19 +0000

    Fix code style (php-cs-fixer)

--- tests/EnumTranslationTest.php
+++ tests/EnumTranslationTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Datomatic\EnumHelper\Tests\Support\Enums\Status;
 
-
 it('return empty string on translate if is not Laravel')
     ->expect(Status::PENDING->translate())
     ->toBe('');
